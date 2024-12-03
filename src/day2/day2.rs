9463fcaaf5ffc5bc93dfd2875d554dfebae33124
Andy <andrewramsier@gmail.com>
2024-12-03 10:34:27 +0000

    i feel ill so obviously im going to write some code

--- src/day2/day2.rs
+++ src/day2/day2.rs
@@ -17,61 +17,37 @@ pub fn task1() {
     let lines_len: i32 = lines.len() as i32;
     let mut unsafe_reports: i32 = 0;
 
-    
-
     for line in lines {
-
-        let cloned_line = line.clone();
-        // println!("{:?}", cloned_line);
-
-        let mut curr_val = "";
-        let mut prev_val = "";
-        
-
-        for l in 1..cloned_line.len() {
-            prev_val = curr_val;
-
-            //increasing
-            if cloned_line[l] > cloned_line[l-1]{
-                curr_val = "asc";
-                if (cloned_line[l] - cloned_line[l-1]) > 3 {
-                    // println!("increasing : {} {} unsafe", cloned_line[l], cloned_line[l-1]);
-                    unsafe_reports+=1;
-                    break;
-                }
-                if curr_val != "" && prev_val != ""{
-                    if curr_val != prev_val{
-                        // println!("Changed direction");
-                        unsafe_reports +=1;
-                        break;
-                    }
-                }
-            }
-            //decreasing
-            else if cloned_line[l] < cloned_line[l-1] {
-                curr_val = "desc";
-                if (cloned_line[l-1] - cloned_line[l]) > 3 {
-                    // println!("decreasing : {} {} unsafe", cloned_line[l-1], cloned_line[l]);
-                    unsafe_reports+=1;
-                    break;
-                }
-                if curr_val != "" && prev_val != ""{
-                    if curr_val != prev_val{
-                        // println!("Changed direction");
-                        unsafe_reports +=1;
-                        break;
-                    }
-                }
-            }
-            else if cloned_line[l] == cloned_line[l-1] {
-                unsafe_reports +=1;
-                break;
-            }
+        if check_safety(&line) > 0{
+            unsafe_reports+=1;
         }
     }
 
     println!("{}", lines_len - unsafe_reports);
     println!("Day 2 Task 1: Elapsed time: {:.2?}", before.elapsed());
+}
+
+fn check_safety(line: &Vec<i32>) -> i32{
+    let asc= line[0] < line[1];
+    let mut unsafe_line = 0;
+    for i in line.windows(2){
+        let (cur, next) = (i[0], i[1]);
+
+        if (cur.abs_diff(next)  ==  0 )|| (cur.abs_diff(next) > 3){
+            // println!("{} {} {} {:?}", cur, next, cur.abs_diff(next), line);
+            unsafe_line+=1;
+        } else if (cur < next) && !asc {
+            // println!("{} {} {} {:?}", cur, next, "!asc", line);
+
+            unsafe_line+=1;
+
+        } else if (cur > next) && asc {
+            // println!("{} {} {} {:?}", cur, next, "asc", line);
+            unsafe_line+=1;
+        }
+            
+    }
+     return unsafe_line;
 }
 
 
@@ -89,65 +65,30 @@ pub fn task2() {
     let lines_len: i32 = lines.len() as i32;
     let mut unsafe_reports: i32 = 0;
 
-    
-
     for line in lines {
-
-
-        fn check_safety(line: &Vec<i32>) -> i32{
-            let asc= line[0] < line[1];
-            let mut unsafe_line = 0;
-            for i in line.windows(2){
-                let (cur, next) = (i[0], i[1]);
-
-                if (cur.abs_diff(next)  ==  0 )|| (cur.abs_diff(next) > 3){
-                    // println!("{} {} {} {:?}", cur, next, cur.abs_diff(next), line);
-                    unsafe_line+=1;
-                } else if (cur < next) && !asc {
-                    // println!("{} {} {} {:?}", cur, next, "!asc", line);
-
-                    unsafe_line+=1;
-
-                } else if (cur > next) && asc {
-                    // println!("{} {} {} {:?}", cur, next, "asc", line);
-                    unsafe_line+=1;
-                }
-                    
-            }
-             return unsafe_line;
-        }
-        
         fn loop_over_line(line: Vec<i32>) -> i32{
             let mut unsafe_line_count = 0;
             let safety_score = check_safety(&line);
             if safety_score > 0{;
                 unsafe_line_count+=1;
            }
-           
-        for l in 0..line.len() {
+            for l in 0..line.len() {
 
-            let mut cloned_line = line.clone();
-            cloned_line.remove(l);
-            let qa = check_safety(&cloned_line);
-            if qa == 0 {
-                if unsafe_line_count > 0 {
-                    unsafe_line_count -=1;
+                let mut cloned_line = line.clone();
+                cloned_line.remove(l);
+                let qa = check_safety(&cloned_line);
+                if qa == 0 {
+                    if unsafe_line_count > 0 {
+                        unsafe_line_count -=1;
+                    }
+                    break;
                 }
-                break;
             }
-
+            return unsafe_line_count;
         }
-        return unsafe_line_count;
+        unsafe_reports += loop_over_line(line);
     }
-    unsafe_reports += loop_over_line(line);
-    }
-
-
     println!("{}", lines_len - unsafe_reports);
     println!("Day 2 Task 2: Elapsed time: {:.2?}", before.elapsed());
-
-
-
-
 }
 
