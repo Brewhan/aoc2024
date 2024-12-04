ec21b06f44cfddb8500d5c20a85f0a36d47c053a
Andy <andrewramsier@gmail.com>
2024-12-04 20:00:10 +0000

    refactor

--- src/day4/day4.rs
+++ src/day4/day4.rs
@@ -17,20 +17,16 @@ use regex::Regex;
 pub fn task1(file_path: &str) {
     let contents = get_file_contents(file_path);
     let mut total: i32 = 0;
-    let re = Regex::new(r"XMAS").unwrap();
     let contents: Vec<String> = contents
         .split("\r\n")
         .map(|s| s.to_string())
         .filter(|s| !s.trim().is_empty())
         .collect();
-
-    //THIS LOOKS FORWARDS AND BACKWARDS UP AND DOWN
-    //rotate_scan(contents.clone(), re.clone(), &mut total);
-    traverse_diagonally(contents.clone(), &mut total);
-    println!("{}", total);
+    traverse(contents.clone(), &mut total);
+    println!("Total: {}", total);
 }
 
-fn traverse_diagonally(contents: Vec<String>, total: &mut i32) {
+fn traverse(contents: Vec<String>, total: &mut i32) {
     //Convert the Vec<String> to a matrix
     let mut matrix: Vec<Vec<char>> = contents.iter().map(|row| row.chars().collect()).collect();
     matrix =add_padding(&matrix);
@@ -96,7 +92,6 @@ fn traverse_diagonally(contents: Vec<String>, total: &mut i32) {
                         && matrix[index][k + 2] == 'A'
                         && matrix[index][k + 3] == 'S'
                     {
-                        println!("{:?}{:?},{:?} r", matrix[index][k], index, k);
                         *total += 1;
                     }
                 }
@@ -125,7 +120,6 @@ fn traverse_diagonally(contents: Vec<String>, total: &mut i32) {
 
                 //DOWN
                 if rows - index >= 3 {
-                    println!("ABC{:?}{:?},{:?} r", matrix[index][k], index, k);
                     if matrix[index + 1][k] == 'M'
                         && matrix[index + 2][k] == 'A'
                         && matrix[index + 3][k] == 'S'
@@ -140,67 +134,6 @@ fn traverse_diagonally(contents: Vec<String>, total: &mut i32) {
     }
     
 }
-
-fn rotate_clockwise(contents: &mut Vec<String>) {
-    let cols = contents.len();
-    let rows = contents[0].len();
-
-    println!("{} {} ", cols, rows);
-
-    let mut rotated = vec![String::new(); cols];
-
-    for col in 0..rows {
-        let mut new_row = String::new();
-        for row in (0..rows).rev() {
-            new_row.push(contents[row].chars().nth(col).unwrap());
-        }
-
-        rotated.push(new_row);
-    }
-    if !rotated.is_empty() {
-        *contents = rotated
-            .into_iter()
-            .filter(|s| !s.trim().is_empty())
-            .collect();
-    }
-}
-
-fn rotate_scan(in_contents: Vec<String>, re: Regex, total: &mut i32) {
-    let mut local_total: i32 = 0;
-    let mut contents = in_contents;
-
-    for _ in 0..1 {
-        for _ in 0..2 {
-            rotate_clockwise(&mut contents);
-            local_total += horizontally(contents.clone(), re.clone());
-        }
-
-        for _ in 0..2 {
-            rotate_clockwise(&mut contents);
-            local_total += horizontally(contents.clone(), re.clone());
-        }
-        reverse_all_rows(&mut contents);
-    }
-
-    *total += local_total;
-}
-
-fn horizontally(contents: Vec<String>, re: Regex) -> i32 {
-    let mut total = 0;
-    for c in contents {
-        let matches: usize = re.find_iter(&c).count();
-        total += matches as i32;
-    }
-    return total;
-}
-
-fn reverse_all_rows(contents: &mut Vec<String>) {
-    *contents = contents
-        .iter()
-        .map(|row| row.chars().rev().collect::<String>())
-        .collect();
-}
-
 
 fn add_padding(matrix: &Vec<Vec<char>>) -> Vec<Vec<char>> {
     let cols = matrix[0].len();
